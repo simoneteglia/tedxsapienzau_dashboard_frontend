612e9168b5845e7f335dd78d0d79c3ac0398ec69
alessio <alessio.scanu01@gmail.com>
2025-10-15 22:11:23 +0200

    add "Informazioni Tirocinio" section

--- client/src/components/pages/VolunteerDetails.jsx
+++ client/src/components/pages/VolunteerDetails.jsx
@@ -81,323 +81,353 @@ export default function VolunteerDetails() {
         className="landing-main-container"
         style={{ padding: "0px 20px 10px 20px", color: "#333" }}
       >
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          marginBottom: "20px",
-        }}
-      >
-        {/* back arrow on the far left */}
-        <button
-          onClick={() => navigate("/volunteers")}
-          style={{
-            background: "none",
-            border: "none",
-            cursor: "pointer",
-            display: "flex",
-            alignItems: "center",
-            color: "#333",
-            fontSize: "18px",
-          }}
-        >
-          <ArrowBackIcon style={{ fontSize: "28px" }} />
-        </button>
-
-        {/* centered title */}
-        <h1
-          className="volunteers-title"
-          style={{ flex: 1, textAlign: "center", margin: 0 }}
-        >
-          Dettagli Volontario
-        </h1>
-
-        {/* empty spacer on the right so title stays centered */}
-        <div style={{ width: "40px" }}></div>
-      </div>
-
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ flex: 1, textAlign: "center" }}>
-          <img
-            src={image}
-            alt="Volunteer"
-            style={{
-              width: "150px",
-              height: "150px",
-              borderRadius: "50%",
-              border: "2px solid #ddd",
-            }}
-          />
-        </div>
-
-        <div style={{ flex: 2, padding: "20px" }}>
-          <h2 style={{ margin: "0" }}>
-            {volunteer.name} {volunteer.surname}
-            {volunteer.subleader === "Si" && (
-              <label
-                style={{
-                  backgroundColor: "#4CAF50",
-                  color: "white",
-                  fontSize: "18px",
-                  padding: "10px 15px",
-                  borderRadius: "5px",
-                  marginLeft: "20px",
-                }}
-              >
-                Subleader
-              </label>
-            )}
-          </h2>
-          <p>
-            <strong>Team:</strong> {volunteer.team || "N/A"}
-          </p>
-          <p>
-            <strong>Status:</strong> {volunteer.status || "N/A"}
-          </p>
-        </div>
-
-        <div style={{ flex: 1, padding: "20px" }}>
-          {volunteer.is_ex_member === "Si" && (
-            <div
-              style={{
-                backgroundColor: "rgba(246, 71,71, 0.8)",
-                borderRadius: "8px",
-                fontSize: "18px",
-                padding: "10px",
-                boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
-              }}
-            >
-              <p>
-                <strong>Ex Socio:</strong> {volunteer.is_ex_member || "N/A"}
-              </p>
-              <p>
-                <strong>Data Dimissione:</strong>{" "}
-                {volunteer.resignation_date || "N/A"}
-              </p>
-            </div>
-          )}
-        </div>
-      </div>
-      <hr />
-      <h3
-        style={{
-          color: "#eb0028",
-        }}
-      >
-        Informazioni Personali
-      </h3>
-      <p>
-        <strong>Genere:</strong> {volunteer.gender || "N/A"}
-      </p>
-      <p>
-        <strong>Telefono:</strong> {volunteer.phone_number || "N/A"}
-      </p>
-      <p>
-        <strong>Email Personale:</strong> {volunteer.personal_email || "N/A"}
-      </p>
-      <p>
-        <strong>Data di Nascita:</strong> {volunteer.date_of_birth || "N/A"}
-      </p>
-      <p>
-        <strong>Luogo di Nascita:</strong> {volunteer.place_of_birth || "N/A"}
-      </p>
-      <p>
-        <strong>Codice Fiscale:</strong> {volunteer.fiscal_code || "N/A"}
-      </p>
-      <p>
-        <strong>Luogo di Residenza:</strong>{" "}
-        {volunteer.place_of_residence || "N/A"}
-      </p>
-      <p>
-        <strong>Indirizzo di Domicilio:</strong>{" "}
-        {volunteer.address_of_domicile || "N/A"}
-      </p>
-      <hr />
-      <h3
-        style={{
-          color: "#eb0028",
-        }}
-      >
-        Informazioni Accademiche
-      </h3>
-      <p>
-        <strong>Sei iscritto in Sapienza?</strong>{" "}
-        {volunteer.is_enrolled_in_sapienza || "N/A"}
-      </p>
-      <p>
-        <strong>Matricola:</strong> {volunteer.student_id || "N/A"}
-      </p>
-      <p>
-        <strong>Email Istituzionale:</strong>{" "}
-        {volunteer.institutional_email || "N/A"}
-      </p>
-      <p>
-        <strong>Status Accademico:</strong> {volunteer.academic_status || "N/A"}
-      </p>
-
-      <p>
-        <strong>Facoltà di Appartenenza:</strong>{" "}
-        {volunteer.faculty_name || "N/A"}
-      </p>
-      <p>
-        <strong>Dipartimento:</strong> {volunteer.dipartimento || "N/A"}
-      </p>
-      <p>
-        <strong>Tipologia:</strong> {volunteer.course_type || "N/A"}
-      </p>
-
-      <p>
-        <strong>Corso:</strong> {volunteer.degree_name || "N/A"}
-      </p>
-
-      <p>
-        <strong>Anno di Iscrizione:</strong>{" "}
-        {volunteer.enrollment_year || "N/A"}
-      </p>
-      <p>
-        <strong>Erasmus/Estero:</strong> {volunteer.erasmus_status || "N/A"}
-      </p>
-      <hr />
-      <h3
-        style={{
-          color: "#eb0028",
-        }}
-      >
-        Associazioni
-      </h3>
-      <p>
-        <strong>Associazione Esterna:</strong>{" "}
-        {volunteer.is_in_external_association || "N/A"}
-      </p>
-      <p>
-        <strong>Nome Associazione:</strong>{" "}
-        {volunteer.external_association_name || "N/A"}
-      </p>
-      <p>
-        <strong>Data di Ingresso in Associazione:</strong>{" "}
-        {volunteer.date_of_joining || "N/A"}
-      </p>
-      <hr />
-      <h3
-        style={{
-          color: "#eb0028",
-        }}
-      >
-        Preferenze ed Extra
-      </h3>
-      <p>
-        <strong>Esigenze Alimentari:</strong>{" "}
-        {volunteer.dietary_requirements || "N/A"}
-      </p>
-      <p>
-        <strong>Taglia T-Shirt:</strong> {volunteer.tshirt_size || "N/A"}
-      </p>
-      <p>
-        <strong>T-Shirt Presa?</strong> {volunteer.has_taken_tshirt || "N/A"}
-      </p>
-      <hr />
-      <h3
-        style={{
-          color: "#eb0028",
-        }}
-      >
-        Documenti
-      </h3>
-      <p>
-        <strong>Documenti Socio:</strong>{" "}
-        {volunteer.id_document_link ? (
-          <a
-            href={volunteer.id_document_link}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "blue", textDecoration: "underline" }}
-          >
-            Visualizza documento
-          </a>
-        ) : (
-          "N/A"
-        )}
-      </p>
-
-      <p>
-        <strong>Tipo di Documento:</strong>{" "}
-        {volunteer.id_document_type || "N/A"}
-      </p>
-      <p>
-        <strong>Numero del Documento:</strong>{" "}
-        {volunteer.id_document_number || "N/A"}
-      </p>
-      <p>
-        <strong>Scadenza Documento:</strong>{" "}
-        {volunteer.id_document_expiry_date || "N/A"}
-      </p>
-      <hr />
-      <h3
-        style={{
-          color: "#eb0028",
-        }}
-      >
-        Note
-      </h3>
-      <p>
-        <strong>Note:</strong> {volunteer.notes || " "}
-      </p>
-
-      {(volunteer.is_ex_member === true || volunteer.is_ex_member === "Si") && (
         <div
           style={{
-            backgroundColor: "rgba(246, 71,71, 0.8)",
-            borderRadius: "8px",
-            fontSize: "18px",
-            padding: "10px",
-            boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: "20px",
           }}
         >
-          <p>
-            <strong>Ex Socio:</strong>{" "}
-            {volunteer.is_ex_member === true
-              ? "Si"
-              : volunteer.is_ex_member || "N/A"}
-          </p>
-          <p>
-            <strong>Data Dimissione:</strong>{" "}
-            {volunteer.resignation_date || "N/A"}
-          </p>
+          {/* back arrow on the far left */}
+          <button
+            onClick={() => navigate("/volunteers")}
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              color: "#333",
+              fontSize: "18px",
+            }}
+          >
+            <ArrowBackIcon style={{ fontSize: "28px" }} />
+          </button>
+
+          {/* centered title */}
+          <h1
+            className="volunteers-title"
+            style={{ flex: 1, textAlign: "center", margin: 0 }}
+          >
+            Dettagli Volontario
+          </h1>
+
+          {/* empty spacer on the right so title stays centered */}
+          <div style={{ width: "40px" }}></div>
         </div>
-      )}
 
-      <hr />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ flex: 1, textAlign: "center" }}>
+            <img
+              src={image}
+              alt="Volunteer"
+              style={{
+                width: "150px",
+                height: "150px",
+                borderRadius: "50%",
+                border: "2px solid #ddd",
+              }}
+            />
+          </div>
 
-      <button
-        style={{
-          backgroundColor: "#FFA500",
-          color: "white",
-          padding: "10px 15px",
-          borderRadius: "5px",
-          cursor: "pointer",
-          marginRight: 8,
-        }}
-        onClick={() => navigate(`/volunteer/edit/${id}`)}
-      >
-        Modifica
-      </button>
+          <div style={{ flex: 2, padding: "20px" }}>
+            <h2 style={{ margin: "0" }}>
+              {volunteer.name} {volunteer.surname}
+              {volunteer.subleader === "Si" && (
+                <label
+                  style={{
+                    backgroundColor: "#4CAF50",
+                    color: "white",
+                    fontSize: "18px",
+                    padding: "10px 15px",
+                    borderRadius: "5px",
+                    marginLeft: "20px",
+                  }}
+                >
+                  Subleader
+                </label>
+              )}
+            </h2>
+            <p>
+              <strong>Team:</strong> {volunteer.team || "N/A"}
+            </p>
+            <p>
+              <strong>Status:</strong> {volunteer.status || "N/A"}
+            </p>
+          </div>
 
-      <button
-        style={{
-          backgroundColor: "#1E90FF", // different color to distinguish
-          color: "white",
-          padding: "10px 15px",
-          borderRadius: "5px",
-          cursor: "pointer",
-          marginRight: 8,
-        }}
-        onClick={() => window.open(volunteer.id_document_link, "_blank")}
-      >
-        Documento
-      </button>
+          <div style={{ flex: 1, padding: "20px" }}>
+            {volunteer.is_ex_member === "Si" && (
+              <div
+                style={{
+                  backgroundColor: "rgba(246, 71,71, 0.8)",
+                  borderRadius: "8px",
+                  fontSize: "18px",
+                  padding: "10px",
+                  boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+                }}
+              >
+                <p>
+                  <strong>Ex Socio:</strong> {volunteer.is_ex_member || "N/A"}
+                </p>
+                <p>
+                  <strong>Data Dimissione:</strong>{" "}
+                  {volunteer.resignation_date || "N/A"}
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Informazioni Personali
+        </h3>
+        <p>
+          <strong>Genere:</strong> {volunteer.gender || "N/A"}
+        </p>
+        <p>
+          <strong>Telefono:</strong> {volunteer.phone_number || "N/A"}
+        </p>
+        <p>
+          <strong>Email Personale:</strong> {volunteer.personal_email || "N/A"}
+        </p>
+        <p>
+          <strong>Data di Nascita:</strong> {volunteer.date_of_birth || "N/A"}
+        </p>
+        <p>
+          <strong>Luogo di Nascita:</strong> {volunteer.place_of_birth || "N/A"}
+        </p>
+        <p>
+          <strong>Codice Fiscale:</strong> {volunteer.fiscal_code || "N/A"}
+        </p>
+        <p>
+          <strong>Luogo di Residenza:</strong>{" "}
+          {volunteer.place_of_residence || "N/A"}
+        </p>
+        <p>
+          <strong>Indirizzo di Domicilio:</strong>{" "}
+          {volunteer.address_of_domicile || "N/A"}
+        </p>
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Informazioni Accademiche
+        </h3>
+        <p>
+          <strong>Sei iscritto in Sapienza?</strong>{" "}
+          {volunteer.is_enrolled_in_sapienza || "N/A"}
+        </p>
+        <p>
+          <strong>Matricola:</strong> {volunteer.student_id || "N/A"}
+        </p>
+        <p>
+          <strong>Email Istituzionale:</strong>{" "}
+          {volunteer.institutional_email || "N/A"}
+        </p>
+        <p>
+          <strong>Status Accademico:</strong>{" "}
+          {volunteer.academic_status || "N/A"}
+        </p>
 
-      <button
+        <p>
+          <strong>Facoltà di Appartenenza:</strong>{" "}
+          {volunteer.faculty_name || "N/A"}
+        </p>
+        <p>
+          <strong>Dipartimento:</strong> {volunteer.dipartimento || "N/A"}
+        </p>
+        <p>
+          <strong>Tipologia:</strong> {volunteer.course_type || "N/A"}
+        </p>
+
+        <p>
+          <strong>Corso:</strong> {volunteer.degree_name || "N/A"}
+        </p>
+
+        <p>
+          <strong>Anno di Iscrizione:</strong>{" "}
+          {volunteer.enrollment_year || "N/A"}
+        </p>
+        <p>
+          <strong>Erasmus/Estero:</strong> {volunteer.erasmus_status || "N/A"}
+        </p>
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Associazioni
+        </h3>
+        <p>
+          <strong>Associazione Esterna:</strong>{" "}
+          {volunteer.is_in_external_association || "N/A"}
+        </p>
+        <p>
+          <strong>Nome Associazione:</strong>{" "}
+          {volunteer.external_association_name || "N/A"}
+        </p>
+        <p>
+          <strong>Data di Ingresso in Associazione:</strong>{" "}
+          {volunteer.date_of_joining || "N/A"}
+        </p>
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Preferenze ed Extra
+        </h3>
+        <p>
+          <strong>Esigenze Alimentari:</strong>{" "}
+          {volunteer.dietary_requirements || "N/A"}
+        </p>
+        <p>
+          <strong>Taglia T-Shirt:</strong> {volunteer.tshirt_size || "N/A"}
+        </p>
+        <p>
+          <strong>T-Shirt Presa?</strong> {volunteer.has_taken_tshirt || "N/A"}
+        </p>
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Documenti
+        </h3>
+        <p>
+          <strong>Documenti Socio:</strong>{" "}
+          {volunteer.id_document_link ? (
+            <a
+              href={volunteer.id_document_link}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "blue", textDecoration: "underline" }}
+            >
+              Visualizza documento
+            </a>
+          ) : (
+            "N/A"
+          )}
+        </p>
+
+        <p>
+          <strong>Tipo di Documento:</strong>{" "}
+          {volunteer.id_document_type || "N/A"}
+        </p>
+        <p>
+          <strong>Numero del Documento:</strong>{" "}
+          {volunteer.id_document_number || "N/A"}
+        </p>
+        <p>
+          <strong>Scadenza Documento:</strong>{" "}
+          {volunteer.id_document_expiry_date || "N/A"}
+        </p>
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Informazioni Tirocinio
+        </h3>
+        <p>
+          <strong>Stato Tirocinio: </strong>{" "}
+          {volunteer.traineeship_status === "ongoing"
+            ? "In corso"
+            : volunteer.traineeship_status === "completed"
+            ? "Terminato"
+            : "Non effettuato"}
+        </p>
+        {(volunteer.traineeship_status === "ongoing" ||
+          volunteer.traineeship_status === "completed") && (
+          <p>
+            <strong>Data Inizio: </strong> {volunteer.start_date || "N/A"}
+          </p>
+        )}
+
+        {volunteer.traineeship_status === "completed" && (
+          <p>
+            <strong>Data Fine: </strong> {volunteer.completed_date || "N/A"}
+          </p>
+        )}
+        <hr />
+        <h3
+          style={{
+            color: "#eb0028",
+          }}
+        >
+          Note
+        </h3>
+        <p>
+          <strong>Note:</strong> {volunteer.notes || " "}
+        </p>
+
+        {(volunteer.is_ex_member === true ||
+          volunteer.is_ex_member === "Si") && (
+          <div
+            style={{
+              backgroundColor: "rgba(246, 71,71, 0.8)",
+              borderRadius: "8px",
+              fontSize: "18px",
+              padding: "10px",
+              boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+            }}
+          >
+            <p>
+              <strong>Ex Socio:</strong>{" "}
+              {volunteer.is_ex_member === true
+                ? "Si"
+                : volunteer.is_ex_member || "N/A"}
+            </p>
+            <p>
+              <strong>Data Dimissione:</strong>{" "}
+              {volunteer.resignation_date || "N/A"}
+            </p>
+          </div>
+        )}
+
+        <hr />
+
+        <button
+          style={{
+            backgroundColor: "#FFA500",
+            color: "white",
+            padding: "10px 15px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            marginRight: 8,
+          }}
+          onClick={() => navigate(`/volunteer/edit/${id}`)}
+        >
+          Modifica
+        </button>
+
+        <button
+          style={{
+            backgroundColor: "#1E90FF", // different color to distinguish
+            color: "white",
+            padding: "10px 15px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            marginRight: 8,
+          }}
+          onClick={() => window.open(volunteer.id_document_link, "_blank")}
+        >
+          Documento
+        </button>
+
+        <button
           style={{
             backgroundColor: "#ff0000ff",
             color: "white",
